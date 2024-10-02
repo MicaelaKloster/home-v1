aeceb2676f65c566e8f0a407be74c8cf3aa6b8ce
Micaela Kloster <108692944+MicaelaKloster@users.noreply.github.com>
2024-10-02 16:57:07 -0300

    correcciones

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = withMT({
   content: [
     './src/**/*.{js,jsx,ts,tsx}',
     './node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-    './node_module/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
